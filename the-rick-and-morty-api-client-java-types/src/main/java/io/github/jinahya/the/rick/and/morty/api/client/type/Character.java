a3bb71ce45ce339fdc7712ce9b9a15894942c05e
Jin Kwon <onacit@gmail.com>
2025-12-09 15:49:57 +0900

    wip

--- the-rick-and-morty-api-client-java-types/src/main/java/io/github/jinahya/the/rick/and/morty/api/client/type/Character.java
+++ the-rick-and-morty-api-client-java-types/src/main/java/io/github/jinahya/the/rick/and/morty/api/client/type/Character.java
@@ -1,23 +1,55 @@
 package io.github.jinahya.the.rick.and.morty.api.client.type;
 
 import jakarta.json.bind.annotation.JsonbVisibility;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
 
 @JsonbVisibility(___NonPrivateVisibilityStrategy.class)
+@Setter(AccessLevel.PROTECTED)
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Character extends __BaseType {
 
-    protected Character() {
-        super();
-    }
+    @JsonbVisibility(___NonPrivateVisibilityStrategy.class)
+    @Setter(AccessLevel.PROTECTED)
+    @Getter
+    @ToString
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class NameAndUrl {
 
-    // -----------------------------------------------------------------------------------------------------------------
-    public int getId() {
-        return id;
-    }
+        private String name;
 
-    void setId(int id) {
-        this.id = id;
+        private String url;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
     private int id;
+
+    private String name;
+
+    private String status;
+
+    private String species;
+
+    private String type;
+
+    private String gender;
+
+    private NameAndUrl origin;
+
+    private NameAndUrl location;
+
+    private String image;
+
+    private List<String> episode;
+
+    private String url;
+
+    private String created;
 }
